a02507248fb91acb8942010bf2f1980890fb0314
Gokul Prasad <gokulprasad125@gmail.com>
2023-01-19 14:44:26 -0800

    added some notebook testing

--- src/classifier.py
+++ src/classifier.py
@@ -2,7 +2,7 @@ import openai
 import pandas as pd
 import os
 
-openai.api_key = os.get_env("openai_api")
+openai.api_key = os.getenv("openai_api")
 
 prompt = "Determine if the following Tweet is True or False: \n"
 

67bd16b9d939e076a0a37cc4501c6d6c3d07c233
Gokul Prasad <gokulprasad125@gmail.com>
2023-03-12 11:24:10 -0700

    fixed classification error

--- src/models/classifier.py
+++ src/models/classifier.py
@@ -102,7 +102,7 @@ def find_relevance(df):
     openai.api_key = os.environ.get("OPENAI_API_KEY")
 
     # run samples
-    if df.shape[0] > 5: 
+    if df.shape[0] > 10: 
         df = df.sample(n = 100)
 
     df["gpt3_answer"] = df["text"].apply(gpt3)
